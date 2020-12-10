25069f1eb11d589acf87ecd99e5e9bb92b0e18bb
ArminZahedani <armin1zahedani@gmail.com>
2020-12-10 11:52:03 +0100

    added Gold star

--- Day10/AOC10.py
+++ Day10/AOC10.py
@@ -15,4 +15,32 @@ for element in newLst:
     previousElement = element
     print(one_jolts, three_jolts)
 three_jolts += 1
-print(one_jolts * three_jolts)
+print("Silver star: {}".format(one_jolts * three_jolts))  # silver
+
+jumps = []
+previousElement = 0
+for element in newLst:
+    jumps.append(element - previousElement)
+    previousElement = element
+subLst = []
+leftPointer, rightPointer = 0, 0
+lastseenElement = 3
+for i in range(0, len(jumps)):
+    element = jumps[i]
+    if element == lastseenElement:
+        subLst.append(jumps[leftPointer: i])
+        leftPointer = i
+        lastseenElement = jumps[i-1]
+subLst.append(jumps[leftPointer:])
+
+count = 1
+for subJmp in subLst:
+    if 3 in subJmp:
+        continue
+    if len(subJmp) == 2:
+        count *= 2
+    if len(subJmp) == 3:
+        count *= 2 ** (len(subJmp) - 1)
+    if len(subJmp) == 4:
+        count *= 7
+print("Gold star: {}".format(count))
