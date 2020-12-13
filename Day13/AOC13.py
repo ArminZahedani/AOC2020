97c72e5cd73317cfbf08751db806735d0ac87d21
ArminZahedani <armin1zahedani@gmail.com>
2020-12-13 11:45:22 +0100

    changed silver + added gold day 13

--- Day13/AOC13.py
+++ Day13/AOC13.py
@@ -1,25 +1,38 @@
 import math
+from sympy.ntheory.modular import crt
 f = open('Day13/AOC13.txt', 'r')
 lst = f.readlines()
 newLst = []
 for element in lst:
     buses = element.replace('\n', '').split(',')
     for bus in buses:
-        if bus != 'x':
-            newLst.append(int(bus))
+        newLst.append(bus)
 
 
 def silver():
-    earliest_time = newLst[0]
+    earliest_time = int(newLst[0])
     earliest_bus_time = -math.inf
     earliest_bus_id = 0
     for bus in newLst[1:]:
-        potential_earliest = (earliest_time % bus) - bus
-        if potential_earliest > earliest_bus_time:
-            earliest_bus_time = potential_earliest
-            earliest_bus_id = bus
+        if bus != 'x':
+            int_bus = int(bus)
+            potential_earliest = (earliest_time % int_bus) - int_bus
+            if potential_earliest > earliest_bus_time:
+                earliest_bus_time = potential_earliest
+                earliest_bus_id = int_bus
     result = earliest_bus_id * abs(earliest_bus_time)
     print(f'Silver star: {result}')
 
 
 silver()
+
+
+def gold():
+    global newLst
+    newLst = newLst[1:]
+    nums = [-i for i in range(0, len(newLst)) if newLst[i] != 'x']
+    lst_m = [int(element) for element in newLst if element != 'x']
+    print(crt(lst_m, nums))
+
+
+gold()
